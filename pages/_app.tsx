3c96b158c1ad6da7ec634522b8152bb59f6bf4c9
macklark <ganesh.kolavennu@gmail.com>
2022-02-12 21:10:55 +0530

    added AuthProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { AuthProvider } from "../utils/authContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <AuthProvider>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AuthProvider>
   );
 }
 
