dc27ede02799ac0a827a76428509bc74bc9c7326
macklark <ganesh.kolavennu@gmail.com>
2022-02-26 12:59:07 +0530

    added data fetching with getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,100 +1,41 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useAuth } from "../utils/authContext";
+import SidebarContent from "../components/sidebarContent";
+import React from "react";
 
 // Chakra UI imports
 import {
   Box,
-  useColorModeValue,
-  BoxProps,
-  FlexProps,
   useDisclosure,
   Flex,
   Text,
   Link,
-  Icon,
   Button,
   Stack,
   Heading,
 } from "@chakra-ui/react";
 
 // React icons import
-import { BiHomeAlt } from "react-icons/bi";
 import { FaPencilAlt } from "react-icons/fa";
-import { AiOutlineTag } from "react-icons/ai";
-
-import { ReactText } from "react";
-import { IconType } from "react-icons";
 import { client } from "../utils/client";
 import { useRouter } from "next/router";
+import { GetStaticProps } from "next";
 
-interface SidebarProps extends BoxProps {
-  onClose: () => void;
-}
+import Feed from "../components/feed";
 
-interface NavButtonProps extends FlexProps {
-  icon: IconType;
-  children: ReactText;
-}
+export const getStaticProps: GetStaticProps = async () => {
+  let { data: blogs, error } = await client.from("blogs").select("*");
 
-// Sidebar component
-const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
-  return (
-    <Box
-      bg={useColorModeValue("white", "gray.900")}
-      borderRight="1px"
-      borderRightColor={useColorModeValue("gray.200", "gray.700")}
-      w={{ base: "full", md: 60 }}
-      position="fixed"
-      height="full"
-      {...rest}
-    >
-      <Flex
-        height="20"
-        alignItems="center"
-        justifyContent="space-between"
-        mx="7"
-      >
-        <Text fontSize="2xl" fontWeight="bold">
-          ⚡ Sharebase
-        </Text>
-      </Flex>
-      <NavButtons icon={BiHomeAlt}>Home</NavButtons>
-      <NavButtons icon={AiOutlineTag}>Tags</NavButtons>
-    </Box>
-  );
-};
+  if (error) {
+    console.log(error);
+  }
 
-const NavButtons = ({ icon, children, ...rest }: NavButtonProps) => {
-  return (
-    <Link
-      href="/tags"
-      style={{ textDecoration: "none" }}
-      _focus={{ boxShadow: "none" }}
-    >
-      <Flex
-        align="center"
-        p="3"
-        mx="4"
-        borderRadius="md"
-        role="group"
-        cursor="pointer"
-        bg={"white"}
-        _hover={{ bg: "cyan.500", color: "white" }}
-        {...rest}
-      >
-        <Icon
-          mr="4"
-          fontSize="16"
-          _hover={{ color: "white" }}
-          as={icon}
-          w={6}
-          h={6}
-        />
-        {children}
-      </Flex>
-    </Link>
-  );
+  return {
+    props: {
+      blogs,
+    },
+  };
 };
 
 const Hero = (): JSX.Element => {
@@ -135,7 +76,11 @@ const Hero = (): JSX.Element => {
   );
 };
 
-const Home: NextPage = () => {
+type blogProps = {
+  blogs: Array<{}>;
+};
+
+const Home: React.FC<blogProps> = ({ blogs }) => {
   const { onClose } = useDisclosure();
   const { isUserAuthenticated } = useAuth();
   const router = useRouter();
@@ -202,7 +147,8 @@ const Home: NextPage = () => {
             )}
           </Flex>
         </Box>
-        <Hero />
+        {isUserAuthenticated === "authenticated" && <Feed blogs={blogs} />}
+        {isUserAuthenticated === "not-authenticated" && <Hero />}
       </Box>
     </>
   );
